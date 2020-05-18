66958b4616d43565d4f202e4c34ce90fbda07829
Tomonari Takai <takai01010100@gmail.com>
2020-05-18 18:49:38 +0900

    名前リストのスタイルを修正

--- src/components/Organism/BindNameList/index.vue
+++ src/components/Organism/BindNameList/index.vue
@@ -1,8 +1,8 @@
 <script src="./index.js"></script>
 <style lang="scss" src="./index.scss" module></style>
 <template lang="pug">
-  div
-    ul
+  div(:class="$style.wrapper")
+    ul(:class="$style.generatedList")
       li(
         v-for="(val, i) in generatedList"
         :key="i"
