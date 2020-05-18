b5de59ad0ff7850855ab9d2db0aaea630ca27119
Tomonari Takai <takai01010100@gmail.com>
2020-05-18 16:08:22 +0900

    名前作成ボタン上のテキストを追加

--- src/components/Organism/BindGenerateButton/index.vue
+++ src/components/Organism/BindGenerateButton/index.vue
@@ -4,6 +4,8 @@
   div(:class="$style.wrapper")
     ul(:class="$style.note")
       li(:class="$style.li") 名前候補は9つになります。
+      li(:class="$style.li") 素材文字：3つ
+      li(:class="$style.li") 名前の長さ：2文字
       li(:class="$style.li") 組み合わせの計算式： 3² = 9通り
     button(
       :class="$style.button"
