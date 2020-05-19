02bef28dca926dfaa00df152948d4e2f039335f5
Tomonari Takai <takai01010100@gmail.com>
2020-05-19 22:33:08 +0900

    名前を分解してリスト作成
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/substring
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/sort

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -24,18 +24,31 @@ export default {
     fetchData() {
       this.theName = this.$route.query.name
     },
+    listFiler(arr) {
+      return arr.filter((val, i, arr) => arr.indexOf(val) === i)
+    },
   },
   computed: {
     alphabetList() {
       const rawList = [...this.theName]
-      const filtered = rawList.filter((val, index, arr) => {
-        return arr.indexOf(val) === index
-      })
-      return filtered
+      return this.listFiler(rawList)
     },
     nameList() {
       const rawData = this.theName
-      return [rawData, ...this.alphabetList]
+      const list = []
+
+      for (let i = 0; i < rawData.length; i++) {
+        for (let ind = rawData.length; ind > i + 1; ind--) {
+          list.push(rawData.substring(i, ind))
+        }
+      }
+
+      const joinedList = this.listFiler([...list, ...this.alphabetList])
+      const result = joinedList.sort((a, b) => {
+        // 文字列の長さでソートする
+        return b.length - a.length
+      })
+      return result
     },
   },
 }
