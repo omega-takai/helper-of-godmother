2988f9fb6b8583b621500ab67012b84e12924b1d
Tomonari Takai <takai01010100@gmail.com>
2020-05-28 01:09:33 +0900

    ナビゲーションのスタイルを変更

--- src/components/Molecule/BaseNavigation/index.vue
+++ src/components/Molecule/BaseNavigation/index.vue
@@ -8,11 +8,13 @@
           iconName="HomeIcon"
           :class="$style.icon"
         )
+        div(:class="$style.text") ホーム
       li(:class="$style.listItem")
         BaseFeatherIcon(
           iconName="ListIcon"
           :class="$style.icon"
         )
+        div(:class="$style.text") リスト
       li(
         :class="$style.listItem"
         @click="onClickEdit()"
@@ -21,14 +23,17 @@
           iconName="EditIcon"
           :class="$style.icon"
         )
+        div(:class="$style.text") つくる
       li(:class="$style.listItem")
         BaseFeatherIcon(
           iconName="RefreshCwIcon"
           :class="$style.icon"
         )
+        div(:class="$style.text") リセット
       li(:class="$style.listItem")
         BaseFeatherIcon(
           iconName="SettingsIcon"
           :class="$style.icon"
         )
+        div(:class="$style.text") 設定
 </template>
