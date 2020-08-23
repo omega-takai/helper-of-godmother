95c3298cd9084688db6bbc9e56c9ae3e3c6f8cc7
Tomonari Takai <takai01010100@gmail.com>
2020-08-23 23:42:02 +0900

    SSR対応: detailページの階層を掘る
    https://ja.nuxtjs.org/api/configuration-generate/#routes

---
+++ src/pages/detail/_name/index.js
@@ -0,0 +1,60 @@
+import NuxtLinkTextWithIcon from '@/components/Molecule/NuxtLinkTextWithIcon/index.vue'
+import LinkButtonWithIcon from '@/components/Molecule/LinkButtonWithIcon/index.vue'
+import BaseTypography from '~/components/Atom/BaseTypography/index.vue'
+
+export default {
+  name: 'detail',
+  components: {
+    BaseTypography,
+    NuxtLinkTextWithIcon,
+    LinkButtonWithIcon,
+  },
+  data() {
+    return {
+      theName: '',
+      kanjiLink: 'https://b-name.jp/赤ちゃん名前辞典/?mode=1&sex=all&t=s&q=',
+      nameList: [],
+      alphabetList: [],
+    }
+  },
+  head() {
+    return {
+      title: this.theName,
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.theName = this.$route.params.name
+    },
+    listFiler(arr) {
+      return arr.filter((val, i, arr) => arr.indexOf(val) === i)
+    },
+  },
+  watch: {
+    theName(newValue) {
+      const list = []
+
+      if (newValue.length <= 0) {
+        this.nameList = list
+      } else {
+        this.alphabetList = this.listFiler([...this.theName])
+
+        for (let i = 0; i < newValue.length; i++) {
+          for (let ind = newValue.length; ind > i + 1; ind--) {
+            list.push(newValue.substring(i, ind))
+          }
+        }
+
+        const joinedList = this.listFiler([...list, ...this.alphabetList])
+        const result = joinedList.sort((a, b) => {
+          // 文字列の長さでソートする
+          return b.length - a.length
+        })
+        this.nameList = result
+      }
+    },
+  },
+}
---
+++ src/pages/detail/_name/index.vue
@@ -0,0 +1,35 @@
+<script src="./index.js"></script>
+<style lang="scss" module src="./index.scss"></style>
+<template lang="pug">
+  transition-group(
+    appear
+    tag="div"
+    :class="$style.wrapper"
+  )
+    div(
+      v-text="theName"
+      key="name"
+      :class="$style.theName"
+    )
+    section(
+      :class="$style.section"
+      key="list"
+    )
+      BaseTypography(
+        :level="3"
+        text="人名漢字を探す（外部リンク）"
+        :class="$style.sectionTitle"
+      )
+      ul(:class="$style.linkList")
+        li(
+          :class="$style.li"
+          v-for="(val, index) in nameList"
+          :key="index"
+        )
+          LinkButtonWithIcon(
+            iconName="ExternalLinkIcon"
+            :text="val"
+            :url="`${kanjiLink}${val}`"
+            :class="$style.button"
+          )
+</template>
